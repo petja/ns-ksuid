425f6760817fc772ad80014937bd95dcd62bd044
petja <hello@petja.me>
2022-02-11 21:30:00 +0200

    Use Prettier

--- index.ts
+++ index.ts
@@ -1,99 +1,85 @@
-import KSUID from "ksuid";
+import KSUID from 'ksuid'
 
-export type IdString<NS extends string> = `${NS}_${string}`;
+export type IdString<NS extends string> = `${NS}_${string}`
 
 export default class NS_KSUID<NS extends string = string> {
-  readonly namespace: NS;
-  readonly ksuid: KSUID;
+  readonly namespace: NS
+  readonly ksuid: KSUID
 
   constructor(namespace: NS, ksuid: KSUID) {
     if (!NS_KSUID.isValidNamespace(namespace)) {
       throw new TypeError(
         `ID namespace must be underscore letters a-z and have length between 1 and 10. Received: "${namespace}"`
-      );
+      )
     }
 
-    this.namespace = namespace;
-    this.ksuid = ksuid;
+    this.namespace = namespace
+    this.ksuid = ksuid
   }
 
   get string(): IdString<NS> {
-    return `${this.namespace}_${this.ksuid.string}` as IdString<NS>;
+    return `${this.namespace}_${this.ksuid.string}` as IdString<NS>
   }
 
   get raw(): Readonly<Buffer> {
-    return this.ksuid.raw;
+    return this.ksuid.raw
   }
 
   get date(): Readonly<Date> {
-    return this.ksuid.date;
+    return this.ksuid.date
   }
 
   get timestamp(): Readonly<number> {
-    return this.ksuid.timestamp;
+    return this.ksuid.timestamp
   }
 
   get payload(): Readonly<Buffer> {
-    return this.ksuid.payload;
+    return this.ksuid.payload
   }
 
   compare(other: NS_KSUID<NS>) {
-    if (this.namespace !== other.namespace)
-      throw new TypeError("Cannot compare IDs from different namespaces");
+    if (this.namespace !== other.namespace) throw new TypeError('Cannot compare IDs from different namespaces')
 
-    return this.ksuid.compare(other.ksuid);
+    return this.ksuid.compare(other.ksuid)
   }
 
   equals(other: NS_KSUID<NS>) {
-    return this.compare(other) === 0;
+    return this.compare(other) === 0
   }
 
-  static randomSync<NS extends string>(
-    namespace: NS,
-    timeInMs?: number | Date
-  ): NS_KSUID<NS> {
-    return new NS_KSUID<NS>(namespace, KSUID.randomSync(timeInMs as any));
+  static randomSync<NS extends string>(namespace: NS, timeInMs?: number | Date): NS_KSUID<NS> {
+    return new NS_KSUID<NS>(namespace, KSUID.randomSync(timeInMs as any))
   }
 
-  static async random<NS extends string>(
-    namespace: NS,
-    timeInMs?: Date | number
-  ): Promise<NS_KSUID<NS>> {
-    return NS_KSUID.randomSync(namespace, timeInMs);
+  static async random<NS extends string>(namespace: NS, timeInMs?: Date | number): Promise<NS_KSUID<NS>> {
+    return NS_KSUID.randomSync(namespace, timeInMs)
   }
 
   static parse<NS extends string>(str: IdString<NS>): NS_KSUID<NS> {
-    const splits = str.split("_");
-    return new NS_KSUID<NS>(splits[0] as NS, KSUID.parse(splits[1]));
+    const splits = str.split('_')
+    return new NS_KSUID<NS>(splits[0] as NS, KSUID.parse(splits[1]))
   }
 
-  static fromParts<NS extends string>(
-    namespace: NS,
-    timeInMs: number,
-    payload: Buffer
-  ): NS_KSUID<NS> {
-    return new NS_KSUID<NS>(namespace, KSUID.fromParts(timeInMs, payload));
+  static fromParts<NS extends string>(namespace: NS, timeInMs: number, payload: Buffer): NS_KSUID<NS> {
+    return new NS_KSUID<NS>(namespace, KSUID.fromParts(timeInMs, payload))
   }
 
   static isValidNamespace<NS extends string>(namespace: NS): boolean {
-    return /^[a-z]{1,10}$/.test(namespace);
+    return /^[a-z]{1,10}$/.test(namespace)
   }
 
-  static validateIdString<NS extends string>(
-    namespace: NS,
-    str: string
-  ): str is IdString<NS> {
-    const parsed = NS_KSUID.parse(str as any);
-    return parsed.namespace === namespace;
+  static validateIdString<NS extends string>(namespace: NS, str: string): str is IdString<NS> {
+    const parsed = NS_KSUID.parse(str as any)
+    return parsed.namespace === namespace
   }
 
   static isValid(buffer: Buffer) {
-    return KSUID.isValid(buffer);
+    return KSUID.isValid(buffer)
   }
 }
 
 export function* IdFactory<NS extends string>(namespace: NS) {
   while (true) {
-    yield NS_KSUID.randomSync(namespace);
+    yield NS_KSUID.randomSync(namespace)
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,106 +1,98 @@
-import { expect } from "chai";
-import ID, { IdFactory } from "..";
+import { expect } from 'chai'
+import ID, { IdFactory } from '..'
 
-it("throw error if incorrect namespace format", () => {
-  expect(() => ID.randomSync("användare")).to.throw();
-});
+it('throw error if incorrect namespace format', () => {
+  expect(() => ID.randomSync('användare')).to.throw()
+})
 
-it("create ID strings in correct format", () => {
+it('create ID strings in correct format', () => {
   for (let i = 0; i < 100; i++) {
-    const id = ID.randomSync("user");
-    expect(id.string).to.match(/^user_[A-z0-9]+$/);
+    const id = ID.randomSync('user')
+    expect(id.string).to.match(/^user_[A-z0-9]+$/)
   }
-});
+})
 
-it("parse ID strings", () => {
-  const idString = "user_24yPTDCR1QRPZITB83lxvgcz7KI";
-  const parsed = ID.parse(idString);
+it('parse ID strings', () => {
+  const idString = 'user_24yPTDCR1QRPZITB83lxvgcz7KI'
+  const parsed = ID.parse(idString)
 
-  expect(parsed.namespace).to.equal("user");
+  expect(parsed.namespace).to.equal('user')
 
-  expect(parsed.date.getTime()).to.equal(
-    new Date("2022-02-11T17:17:38.000Z").getTime()
-  );
+  expect(parsed.date.getTime()).to.equal(new Date('2022-02-11T17:17:38.000Z').getTime())
 
-  expect(parsed.timestamp).to.equal(244599858);
+  expect(parsed.timestamp).to.equal(244599858)
 
-  expect(parsed.payload.toString("hex")).to.equal(
-    "76e8fbc01cd81a5b6120178d5d76e9fe"
-  );
+  expect(parsed.payload.toString('hex')).to.equal('76e8fbc01cd81a5b6120178d5d76e9fe')
 
-  expect(parsed.raw.toString("hex")).to.equal(
-    "0e944c3276e8fbc01cd81a5b6120178d5d76e9fe"
-  );
+  expect(parsed.raw.toString('hex')).to.equal('0e944c3276e8fbc01cd81a5b6120178d5d76e9fe')
 
-  expect(parsed.string).to.equal(idString);
-});
+  expect(parsed.string).to.equal(idString)
+})
 
-it("throw error when comparing ids from different namespaces", () => {
-  const id1 = ID.randomSync("user");
-  const id2 = ID.randomSync("payment");
-  const id3 = ID.randomSync("payment");
+it('throw error when comparing ids from different namespaces', () => {
+  const id1 = ID.randomSync('user')
+  const id2 = ID.randomSync('payment')
+  const id3 = ID.randomSync('payment')
 
   // @ts-expect-error
-  expect(() => id1.compare(id2)).to.throw();
+  expect(() => id1.compare(id2)).to.throw()
 
-  expect(id2.compare(id3)).to.be.a("number");
-});
+  expect(id2.compare(id3)).to.be.a('number')
+})
 
-it("do equality check", () => {
-  const id1 = ID.randomSync("article");
-  const id2 = ID.randomSync("article");
-  const id3 = ID.parse(id2.string);
+it('do equality check', () => {
+  const id1 = ID.randomSync('article')
+  const id2 = ID.randomSync('article')
+  const id3 = ID.parse(id2.string)
 
-  expect(id1.equals(id2)).to.be.false;
-  expect(id2.equals(id3)).to.be.true;
-});
+  expect(id1.equals(id2)).to.be.false
+  expect(id2.equals(id3)).to.be.true
+})
 
-it("create id async", async () => {
-  const id = await ID.random("car");
-  expect(id.namespace).to.equal("car");
-});
+it('create id async', async () => {
+  const id = await ID.random('car')
+  expect(id.namespace).to.equal('car')
+})
 
-it("create id from parts", () => {
+it('create id from parts', () => {
   const id = ID.fromParts(
-    "person",
-    new Date("2022-02-11T17:17:38.000Z").getTime(),
-    Buffer.from("76e8fbc01cd81a5b6120178d5d76e9fe", "hex")
-  );
+    'person',
+    new Date('2022-02-11T17:17:38.000Z').getTime(),
+    Buffer.from('76e8fbc01cd81a5b6120178d5d76e9fe', 'hex')
+  )
 
-  expect(id.string).to.equal("person_24yPTDCR1QRPZITB83lxvgcz7KI");
-});
+  expect(id.string).to.equal('person_24yPTDCR1QRPZITB83lxvgcz7KI')
+})
 
-it("validate id string", () => {
-  expect(ID.validateIdString("player", "player_24yPHHYrYySQDhHsqEAFjCtuS9d")).to
-    .be.true;
+it('validate id string', () => {
+  expect(ID.validateIdString('player', 'player_24yPHHYrYySQDhHsqEAFjCtuS9d')).to.be.true
 
-  expect(ID.validateIdString("person", "player_24yPHHYrYySQDhHsqEAFjCtuS9d")).to
-    .be.false;
+  expect(ID.validateIdString('person', 'player_24yPHHYrYySQDhHsqEAFjCtuS9d')).to.be.false
 
-  expect(() => ID.validateIdString("player", "player_iNvAlIdVaLue")).to.throw();
-});
+  expect(() => ID.validateIdString('player', 'player_iNvAlIdVaLue')).to.throw()
+})
 
-it("validate buffer", () => {
-  const buffer = Buffer.from("foo");
-  expect(ID.isValid(buffer)).to.be.false;
-});
+it('validate buffer', () => {
+  const buffer = Buffer.from('foo')
+  expect(ID.isValid(buffer)).to.be.false
+})
 
-it("create ids with factory", () => {
-  const idFactory = IdFactory("person");
+it('create ids with factory', () => {
+  const idFactory = IdFactory('person')
 
-  const id1 = idFactory.next().value!;
-  const id2 = idFactory.next().value!;
-  const id3 = idFactory.next().value!;
+  const id1 = idFactory.next().value!
+  const id2 = idFactory.next().value!
+  const id3 = idFactory.next().value!
 
-  expect(id1).to.not.be.undefined;
-  expect(id2).to.not.be.undefined;
-  expect(id3).to.not.be.undefined;
+  expect(id1).to.not.be.undefined
+  expect(id2).to.not.be.undefined
+  expect(id3).to.not.be.undefined
 
-  expect(id1.namespace).to.equal("person");
-  expect(id2.namespace).to.equal("person");
-  expect(id3.namespace).to.equal("person");
+  expect(id1.namespace).to.equal('person')
+  expect(id2.namespace).to.equal('person')
+  expect(id3.namespace).to.equal('person')
 
-  expect(id1.string).to.not.equal(id2.string);
-  expect(id2.string).to.not.equal(id3.string);
-  expect(id3.string).to.not.equal(id1.string);
-});
+  expect(id1.string).to.not.equal(id2.string)
+  expect(id2.string).to.not.equal(id3.string)
+  expect(id3.string).to.not.equal(id1.string)
+})
